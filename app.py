c7ae50a4cea2e7f360ac2fe57f33e51618e707d5
iMuffles <47479217+iMuffles@users.noreply.github.com>
2021-08-16 08:57:56 +1000

    add test route to handle model output

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -13,3 +13,24 @@ def models():
 @ app.route('/methodology')
 def methodology():
     return render_template('methodology.html')
+
+@ app.route('/test_output', methods=['POST'])
+def test_output():
+
+    # Get data from form
+    try:
+        num_cows = int(request.form['test_input'])
+    except ValueError: # ideally validate in frontend, this method for testing only
+        return render_template('models.html', response="Bad input, must be integer")
+
+    # A model
+    water_consumption = num_cows * 44.5
+
+    # Build response dict
+    response = {
+        "num_cows": num_cows,
+        "water_consumption": water_consumption
+    }
+
+    # Serve modelled value to html template
+    return render_template('models.html', response=response)
