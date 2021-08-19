4bcc76c63e649dc6ec10fdb882f583063146e8b5
shyamsrini364 <68639814+shyamsrini364@users.noreply.github.com>
2021-08-19 22:12:56 +1000

    Create tree.R

---
+++ data/tree.R
@@ -0,0 +1,14 @@
+
+irrigation="irrigation_table.csv"
+train.row=sample(1:nrow(irrigation),0.7*nrow(irrigation))
+irri.train=irrigation[train.row,]
+irii.test=irrigation[-train.row]
+  
+fit_tree=tree(Method~.,data=irri.train)
+summary(fit_tree)
+plot(fit_tree)
+text(fit_tree,pretty=0)
+  
+##Testing the model
+tpredict=predict(fit_tree,irri.test,type="class")
+table(actual=irri.test,predicted=tpredict)
