8b3c70701921ac1ed768817bc33942ca12713a4a
iMuffles <47479217+iMuffles@users.noreply.github.com>
2021-08-23 21:53:51 +1000

    add input / irrigation attribute matching function

--- app.py
+++ app.py
@@ -1,14 +1,19 @@
 from flask import Flask, render_template, request
 import json
 import os
+import csv
+from pprint import pprint
 
 app = Flask(__name__)
 
 # DATA LOADING GOES HERE
 dirname = os.path.dirname(__file__)
-with open(os.path.join(dirname, 'data/irrigation_systems.json'), 'r') as f:
+with open(os.path.join(dirname, 'data/irrigation_systems.json'), 'r', encoding='utf-8-sig') as f:
     irrigation_data = json.load(f)
-
+with open(os.path.join(dirname, 'data/Irrigation_table - clean numeric ranges_fixed.csv'), 'r', encoding='utf-8-sig') as f:
+    irrigation_table = list(csv.reader(f))
+with open(os.path.join(dirname, 'data/Irrigation_table.csv'), 'r', encoding='utf-8-sig') as f:
+    irrigation_table_raw = list(csv.reader(f))
 
 # FUNCTIONS GO HERE
 def water_model(cows):
@@ -30,8 +35,43 @@ def water_model(cows):
     return water_consumption
 
 
-# def irrigation_model():
+def irrigation_recommendation(input_dict):
+    """Recommend an irrigation system"""
 
+    # Range attributes
+    fields = ['capital_cost', 'pumping_cost',
+              'labour_requirements', 'efficiency', 'uniformity']
+
+    # Format user input values to list
+    user_values = [input_dict[x] for x in fields]
+    user_values.append(input_dict.getlist("water_loss"))
+
+    # Loop through irrigation systems and rank
+    recommendations = []
+    for system in irrigation_table[1:]:
+        current_sys_recs = []
+        # Range measures
+        for index in range(1, 6):
+            if '-' in system[index]:  # range
+                print(system[index])
+                if int(user_values[index - 1]) in range(int(system[index][0]), int(system[index][3]) + 1):
+                    current_sys_recs.append('green')
+                else:
+                    current_sys_recs.append('red')
+            elif len(system[index]) == 1:
+                if user_values[index - 1] == system[index]:
+                    current_sys_recs.append('green')
+                else:
+                    current_sys_recs.append('red')
+            else:
+                raise Exception
+
+        recommendations.append({
+            'name': system[0],
+            'matches': current_sys_recs,
+            'score': len([x for x in current_sys_recs if x == "green"])})
+
+    return recommendations
 
 # ROUTES GO HERE
 @app.route('/')
@@ -75,22 +115,40 @@ def test_output():
 
 @app.route('/irrigation_proto', methods=['POST'])
 def irrigation_proto():
-    """Prototype irrigation model handler"""
+    """Prototype irrigation model handler
 
-    # Get data from form
-    print(request.form)
+    Response dictionary is a list of dicts. Each dict format:
 
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-    # Reuben's test data for model outputs
-    # Feel free to delete when real thing is ready
-    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-    irrigations = list(irrigation_data.keys())
-    li_of_data = []
-    for irrigation in irrigations:
-        li_of_data.append(irrigation_data[irrigation])
-    response = {
-        'irrigation_system': li_of_data,
-        'field_values': '',
+    {
+        "name": key name used in .csv files
+        "fields": list of descriptive form of attributes from uncleaned table
+        "matches": list that records how the input matched the attribute - 
+                   green for a match, red for a non-match, 
+                   corresponds to fields list
+        "score": total number of matches
+        "data": dictionary from irrigation_systems.json
     }
-    return render_template('models.html', response=response)
+
+    Naive Bayes could replace "score"
+    """
+
+    # # Uncomment to see request data format
+    # pprint(request.form)
+
+    # Get recommendations based on form data
+    recs = irrigation_recommendation(request.form)
+
+    # Join with other data for output
+    for rec in recs:
+        rec['data'] = irrigation_data[rec['name']]
+        rec['fields'] = [x for x in irrigation_table_raw if x[0]
+                         == rec['name']][0][1:6]
+
+    # Sort recommendations by score
+    recs.sort(key=lambda x: -int(x['score']))
+
+    # # Uncomment to see recommendation data format
+    # pprint(recs[0])
+
+    # Render template
+    return render_template('models.html', response=recs)
